d291ad75bf30d75ecbb70d5912670c914b229779
iatig <80623099+iatig@users.noreply.github.com>
2024-02-08 14:16:32 +0800

    Initial commit

---
+++ example1.py
@@ -0,0 +1,272 @@
+#!/usr/bin/python3
+#----------------------------------------------------------
+#
+#                  example1.py
+#             =======================
+#
+#  This program uses the qBP algorithm to contract 
+#  a PEPS in the shape of the example given in the document describing
+#  blockBP, in Sec.2.2
+#
+#  It creates a random PEPS with the topology of the TN in the example,
+#  and calculates the <psi|psi> doubel-edge TN from it. Then it gives it
+#  to the blockBP and uses the converged messages to calculate calculate
+#  the expectation value <psi|Z_0|psi>.
+#
+#  It compares it to the expectation values from ncon. 
+#
+# History:
+# ---------
+#
+# 8-Feb-2024 --- Initial version (copied from the blockBP example1.py)
+#
+#----------------------------------------------------------
+#
+
+
+import numpy as np
+
+from numpy.linalg import norm, svd, qr
+
+from numpy import zeros, ones, array, tensordot, sqrt, diag, dot, \
+	reshape, transpose, conj, eye, trace, pi
+	
+from sys import exit
+
+from qbp import qbp
+
+from ncon import ncon
+
+import TenQI
+
+
+try:
+	from mpi4py import MPI
+except ImportError as e:
+	pass  # module doesn't exist, deal with it.
+	MPI=None
+
+
+#
+# ------------------------- create_example  ---------------------------
+#
+
+def create_example_TN(d, D):
+	
+	"""
+	 We create a random PEPS according to the example in Sec. 2.2 in the 
+	 blockBP document, which has 12 spins.
+	 
+	 The physical legs are then contracted together in the <psi|psi> TN 
+	 to create a double edge TN, on which we define the parameters that
+	 are needed for blockBP contraction.
+	 
+	 Output:
+	 ===========
+	 1. The tensors of the original PEPS (single-edges)
+	 
+	 2. The blockBP parameters: 
+	 
+	   T_list, edges_list, pos_list, blocks_v_list, sedges_dict, 
+	   sedges_list, blocks_con_list
+	
+	"""
+	
+	         
+	D2 = D*D # Bond dimension of the double-edge model
+	         
+	
+	
+	#                T0        T1       T2       T3          T4
+	edges_list = [ [1,2], [1,3,19,6], [5,3], [2,5,4,14], [19,4,7,8,20], 
+	#   T5           T6        T7          T8               9
+	[6,7,10], [8,10,9,11], [9,12], [11,20,15,18,17], [16,14,15],
+	#  10            11
+	[13,17,12], [16,18,13]]
+	
+	n = len(edges_list)
+	
+	# The list of the original PEPS tensors. First index in each tensor
+	# is the physical leg.
+	T_PEPS_list = []
+
+	#
+	# The list of the <psi|psi> TN tensors, which is sent to blockBP
+	#
+	T_list = []
+
+	#
+	# Create the local tensors as random tensors. Here we only use 
+	# real tensors, for otherwise qbp will have problems converging
+	# because of the small loops in the TN.
+	#
+	
+	for eL in edges_list:
+		k = len(eL)
+		sh = [d] + [D]*k
+		
+		T = np.random.normal(size=sh) 
+		T = T/norm(T)
+		
+		T_PEPS_list.append(T)
+		
+		#
+		# Contract T with T^* and fuse the corresponding virtual legs 
+		#
+		
+		perm=[]
+		for i in range(k):
+			perm = perm + [i,i+k]
+	
+		T2 = tensordot(T, conj(T), axes=([0],[0]))
+		T2 = T2.transpose(perm)
+		T2 = T2.reshape([D2]*k)
+		
+		T_list.append(T2)
+		
+		
+	
+	return T_PEPS_list, T_list, edges_list
+
+
+#
+#
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#
+#                             M  A  I  N
+#
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#
+#
+
+
+def main():
+	
+	random_seed = 2
+	
+	d = 2            # Physical bond dim
+	D = 5            # Logical bond dim
+	
+	obs = array([[0.6,0.2+0.1j],[0.2-0.1j,0.1]])  # Observable we calculate
+	
+	ket = True       # Whether or not to use ket tensors
+	
+	#
+	# Parameters for stopping condition and convergence
+	#
+	delta_err      = 1e-4  
+	max_iterations = 100
+	damping_rate   = 0.1
+	
+	#
+	#  ~~~~~~~~~~~~~~~~~~~  Program starts here  ~~~~~~~~~~~~~~~~~~~~~~
+	#
+	
+
+
+	np.random.seed(random_seed)
+			
+	#
+	# Get the TN. The T_PEPS_list are the ket tensors (first leg is 
+	# the physical leg of dim d). T_list are the ket-bra tensors
+	#
+	T_PEPS_list, T_list, edges_list = create_example_TN(d, D)
+
+	T0 = T_PEPS_list[0]
+	
+	#	
+	# ================================================================
+	# (1) Use qBP to calculate the expectation value of obs at i=0
+	# ================================================================
+	#
+
+	if ket:
+		BP_tensors = T_PEPS_list
+	else:
+		BP_tensors = T_list
+
+	m_list, err, iter_no = qbp(BP_tensors, edges_list,  \
+			max_iter=max_iterations, delta=delta_err, \
+			damping=damping_rate, use_ket=ket)
+		
+	print(f"... done with {iter_no} iterations and BP messages error={err}")
+
+	print("\n\n\n")
+	
+
+	#
+	# Once the qBP is over, we use the converged messages to calculate
+	# <Z_0> --- the magnetization of qubit 0
+	#
+	# Note that vertex 0 has two neighboring vertices - 1,3.
+	#
+	
+	if not ket:
+		m_1_to_0 = m_list[1][0].reshape([D,D])
+		m_3_to_0 = m_list[3][0].reshape([D,D])
+	else:
+		m_1_to_0 = m_list[1][0]
+		m_3_to_0 = m_list[3][0]
+	
+
+	# T0 legs are [d,D1, D3]
+
+	#
+	# First, contract ket legs
+	# 
+	rho = tensordot(m_1_to_0, T0, axes=([0],[1])) 
+	
+	# now rho legs are [D1*, d, D3]
+	
+	rho = tensordot(rho, m_3_to_0, axes=([2],[0]))
+	
+	# now rho legs are [D1*, d, D3*]
+	
+	# normalize rho (so that Tr=1)
+	
+	rho = tensordot(rho, conj(T0), axes=([0,2], [1,2]))
+	
+	rho = rho/np.trace(rho)
+	
+	av_qBP = np.trace(rho@obs)
+
+	#	
+	# ================================================================
+	# (2) Use ncon to calculate the expectation value of obs at i=0
+	# ================================================================
+	#
+	
+	D2 = D*D
+	
+	newT0 = tensordot(obs, T0, axes=([0],[0]))
+	T = tensordot(newT0, conj(T0), axes=([0],[0]))
+	T = T.transpose([0,2,1,3])
+	T = T.reshape([D2,D2])
+	
+	#
+	# calculate <psi|psi>
+	#
+	denominator = ncon(T_list, edges_list)
+	
+	#
+	# Calculate <psi|obs|psi>
+	#
+	T_list[0] = T
+	enumerator = ncon(T_list, edges_list)
+	
+	av_ncon = enumerator/denominator
+
+
+	#	
+	# ================================================================
+	# (3) Print the results
+	# ================================================================
+	#
+
+	print("Contraction Results:")
+	print("---------------------\n")
+	
+	print(f"ncon: {av_ncon.real:.6g}    qBP: {av_qBP.real:.6g}")
+
+	
+main()
---
+++ qbp.py
@@ -0,0 +1,474 @@
+########################################################################
+#
+#   qbp.py --- Plain BP for tensor networks
+#   ===================================================================
+#
+#
+#  Changelog
+#  ----------
+#
+#  8-Feb-2024: Itai  Initial commit. Added the ket option.
+#
+#
+#
+#
+#
+
+
+import numpy as np
+
+from numpy.linalg import norm
+from numpy import sqrt, tensordot, array, zeros, conj
+
+
+
+#
+# ----------------------- insideout_ket --------------------------------
+#
+
+def insideout_ket(T, in_m_list):
+	
+	"""
+	
+	Given a ket tensor T and a list of incoming messages, calculate the 
+	outgoing BP messages
+	
+	Input Parameters:
+	-------------------
+	
+	T         --- The ket tensors. Should be of the form [d, D0, D1, D2, ...]
+	
+	in_m_list --- The incoming messages. If T has k logical legs, then
+	              there would be k incoming messages, ordered by the legs.
+	              
+	              Each incoming message is a PSD [D_i,D_i] matrix.
+	              
+	              
+	              
+	Output:
+	--------
+	
+	The list of outgoing messages, ordered according to the legs of T.
+	
+	
+	"""
+	
+	legs_no = len(T.shape)-1
+	
+	
+	#
+	# We first create a list of partial contractions of ket-T to the 
+	# messages, and bra-T to the messages. The outgoing messages will
+	# then be the result of cross-contractions of tensors between these
+	# two lists.
+	#
+	
+	ketT = T
+	braT = conj(T)
+	
+	ket_Ts = [ketT]
+	bra_Ts = [braT]
+	
+	#
+	# Contract messages legs_no-1, legs_no-1, ..., 1 to T_ket  AND
+	# 1, ..., legs_no-1 to Tbra
+	#
+	
+	for leg in range(legs_no-1):
+		
+		ketT = tensordot(ketT, in_m_list[legs_no-1-leg], axes=([legs_no-leg],[0]))
+		braT = tensordot(braT, conj(in_m_list[leg]), axes=([1],[1]))
+
+		ket_Ts.append(ketT)
+		bra_Ts.append(braT)
+		
+
+	#
+	# An example of the final lists with legs_no=4:
+	# ----------------------------------------------
+	#
+	# j' = a contracted leg j
+	#
+	# ket_Ts: {[0,1,2,3,4], [0,1,2,3,4'], [0,1,2,4',3'], [0,1,4',3',2']}
+	#
+	# bra_Ts: {[0,1,2,3,4], [0,2,3,4,1'], [0,3,4,1',2'], [0,4,1',2',3']}
+	#
+	# So to get out-message i contract ket_Ts[legs_no-i-1] <==> bra_Ts[i]
+	#
+	# bra: [0,1,2,3,4],   [0,2,3,4,1'],  [0,3,4,1',2'], [0,4,1',2',3']
+	# 	        ||            ||             ||             ||
+	# ket: [0,1,4',3',2'] [0,1,2,4',3'], [0,1,2,3,4'],  [0,1,2,3,4]
+	#        (0,4,3,2)      (0,4,3,2)
+	
+	
+	#
+	# Once we have the ket_Ts list and the bra_Ts list, we cross-contract
+	# them to create the outgoig message:
+	#
+	# Out-going-message[i] := cont( bra[i] + ket[legs-i] )
+	#
+	#
+	out_m_list = []
+	
+	full_axes=list(range(legs_no+1))
+	
+	bra_axes = [0] + list(range(2, legs_no+1))
+	
+	invL = [0] + list(range(legs_no, 1, -1))
+	
+	for i in range(legs_no):
+		
+		ket_axes = invL[:(legs_no-i)] + list(range(1, i+1))
+		
+		out_m = tensordot(ket_Ts[legs_no-i-1], bra_Ts[i], axes=(ket_axes, bra_axes))
+		
+		out_m_list.append(out_m)
+		
+	return out_m_list
+	
+
+
+
+#
+# ----------------------- insideout --------------------------------
+#
+
+def insideout(T, in_m_list, direction='A'):
+	
+	"""
+	
+	Takes a tensor T_{i_0,..., i_{k-1}} and the set of *incoming* messages
+	m_{i_0}, ..., m_{i_{k-1}} to each one of its legs, and computes the 
+	set of outgoing messages.
+	
+	The computation is done in a recursive way. There are two *internal*
+	modes of operation to the routine:
+	'D' (Descending) --- This gives the outgoing message i_0.
+	                     It is done by contracting T with m_{i_{k-1}}, 
+	                     and recursively calling it on the resultant 
+	                     tensor with 'D' mode.
+	                     
+	'A' (Ascending) --- Here we compute all the messages. This is done 
+											by first invoking the routine with 'D' on the
+											tensor, thereby getting the i_0 outgoing message.
+											Then, we contract the i_0 leg, get the tensor
+											T_{i_1, ..., i_{k-1}} and invoke the routine 
+											recursively on T with mode 'A'.
+	
+	Parameters:
+	------------
+	
+	T         --- The tensor on which we act
+	
+	in_m_list --- List of messages. The i'th message is a vector whose 
+	              dimension should corrspond to the dimension of the i'th
+	              leg of T.
+	              
+	direction --- Either 'A' (Ascending) or 'D' (Descending).
+	
+	              NOTE: This is an *internal* parameter, used in the 
+	                    recursion. It should not be set when the function
+	                    is called from the upper-most level.
+	
+	
+	Output:
+	--------
+	The list of outgoing messages.
+	
+	"""
+	
+	legs_no = len(T.shape)
+	
+	if legs_no==1:
+		
+		out_m_list = [T]
+	
+	
+	elif direction=='D':
+	#
+	# direction='D': contract the last leg, and call recursively with 'D'
+	#
+
+		m = in_m_list[-1]
+		
+		T1 = tensordot(T,m, axes=([legs_no-1],[0]))
+			
+			
+		out_m_list = insideout(T1, in_m_list[:(legs_no-1)], 'D')
+		
+	else:
+	#
+	# direction='A': Calculate the outgoing message of i_0, and then 
+	#                contract i_0 and call recuresively with 'A' to 
+	#                obtain the messages of i_1, i_2, ...
+	#
+
+		out_m_list1 = insideout(T, in_m_list, 'D')  
+	
+		m = in_m_list[0]
+		T1 = tensordot(T,m, axes=([0],[0]))
+		out_m_list2 = insideout(T1, in_m_list[1:], 'A') 
+		
+		out_m_list = out_m_list1 + out_m_list2
+		
+		
+	
+	return out_m_list
+	
+	
+	
+
+#
+# --------------------------- qbp --------------------------------
+#
+
+def qbp(T_list, edges_list, m_list=None, max_iter=10000, \
+	delta=1e-6, damping=0.2, permute_order=False, use_ket=False):
+
+	"""
+	
+	Runs a simple BP (belief Propagation) on a (closed) tensor network and 
+	obtains the converged messages. If the TN is a <bra|ket> of PEPS, then
+	the converged messages can be used to approximate the local 
+	environment of the spins. This approximation is equal to the
+	approximation of the simple update method.
+	
+	The TN is described in the notation of the ncon routine: a list of 
+	tensors, together with a corresponding list of labels of edges. Here, 
+	since we don't have any external edges, all labels must be positive 
+	integers.
+	
+	Parameters:
+	-----------
+	
+	T_list --- The list of tensors that make up the TN (like in ncon)
+	
+	edges_list ---	The labels of the edges (legs) of each tensor. This
+									is a list of lists. For each tensor T with k legs
+									there is a list [e_0, e_1, ..., e_{k-1}] of the labels
+									of the legs. The same label must appear in the other
+									tensor with which the leg is contracted.
+	               
+									Each label must be a positive integer.
+	               
+	m_list   ---		An optional initial list of messages. This is a double 
+									list. m_list[i][j] is the message i->j. This is a 
+									vector whose dimension should match that of the leg
+									that connects T_i with T_j.
+									
+									If m_list is not given, qbp starts with messages that
+									are made from normalized random vectors with positive
+									entries.
+									
+	max_iter ---		A upper bound for the maxmial number of iterations
+	
+	delta			---			The distance between two consequtive sets of messages
+									after which the iteration stops (the messages are 
+									considered converged)
+									
+	damping --- 		A possible damping parameter to facilitate convergence.
+									The new message is obtained by:
+									
+									(1-damping)*new_m + damping*old_m
+									
+									
+	permute_order --- If True, then the messages at each round are 
+	                  updated in a random permutation of the vertices. 
+	                  Otherwise, it is done sequentially.
+	                  
+	use_ket     --- Whether or not the tensors are given as ket tensors.
+	                In such case, the first leg (leg 0) is always the 
+	                physical leg. The BP then contract the ket+bra 
+	                online. In such case, the output messages are 
+	                [D,D] matrices.
+	                  
+	
+	Output:
+	-------
+	
+	The list of converged messages. This is a double list m_list[i][j]
+	which holds the i->j message.
+	
+	When use_ket=True, the each message is a matrix [D,D]. Otherwise, 
+	It is a vector [D]
+
+	
+	
+	
+	"""
+
+	log = True
+	
+	#
+	# First, create a dictonary that tells the vertices of each edge
+	# For positive (internal) edge, the value of the dictonary is (i,j), 
+	# where i,j are the vertices connected by it. For negative edges
+	# its (i,i).
+	#
+	
+	vertices = {}
+		
+	n = len(T_list)
+	
+	for i in range(n):
+		i_edges = edges_list[i]
+		for e in i_edges:
+						
+			if e in vertices:
+				(j1,j2) = vertices[e]
+				vertices[e] = (i,j1)
+			else:
+				vertices[e] = (i,i)
+				
+	if log:
+		print("vertices of edges:")
+		print(vertices)
+		
+
+		
+	#
+	# If m_list is empty, create an initial list of messages, where each
+	# message is a normalized random vector with positive entries
+	#
+	
+	if m_list is None:
+
+		#
+		# initialize a 2D list of size n\times n which holds the i->j
+		# message
+		#
+		
+		m_list = [ [None]*n for i in range(n)]  
+		
+		#
+		# Go over all tensors, and for each tensor i go over its legs, and 
+		# find its neighbors j
+		#
+		for i in range(n):
+			
+			no_legs = len(edges_list[i])
+			
+			for leg in range(no_legs):
+				
+					
+				e = edges_list[i][leg]
+				
+				(i1,j1) = vertices[e]
+				
+				j = (i1 if i1 !=i else j1)
+				
+						
+				#
+				# Now assign a normalized random message. When in a ket mode, 
+				# the message is a PSD [D,D] matrix. Otherwise it is a positive 
+				# D vector.
+				#
+				
+				if use_ket:
+					D = T_list[i].shape[leg+1]
+					ms = np.random.normal(size=[D,D])
+					message = ms@ms.T
+				else:
+					D = T_list[i].shape[leg]					
+					message = abs(np.random.normal(size=D))
+					
+				m_list[i][j] = message/norm(message)
+				
+	
+	#
+	# ===================== Main Iteration Loop =========================
+	#
+	
+	err = 1.0
+	iter_no = 0
+	
+	if log:
+		
+		if use_ket:
+			mode_s='ket'
+		else:
+			mode_s = 'ket-bra'
+		
+		print("\n\n")
+		print(f"Entering main qbp loop (in {mode_s} mode)")
+		print("-----------------------------------------\n")
+
+	while err>delta and iter_no < max_iter:
+		
+		iter_no += 1
+
+		err = 0.0
+		
+		#
+		# ---------------------------------------------------------------
+		# Go over all vertices, and for each vertex find its outgoing 
+		# messages from its incoming messages.
+		# ---------------------------------------------------------------
+		#
+		
+		#
+		# If permute_order = True, then the order in which we calculate
+		# the messages is random (we go over a random ordering of the 
+		# vertices)
+		#
+		if permute_order:
+			vertices_order = np.random.permutation(n)
+		else:
+			vertices_order = range(n)
+			
+		for i in vertices_order:
+				
+			T = T_list[i]
+			legs_no = len(edges_list[i])
+						
+			# 
+			# create a list of incoming messages
+			#
+			in_m_list = []   
+			for l in range(legs_no):
+				e = edges_list[i][l]
+				(i1,j1) = vertices[e]
+				j = (i1 if i1 !=i else j1)
+				in_m_list.append(m_list[j][i])
+				
+				
+			#
+			# Calculate the outgoing messages using the insideout routine
+			# (either in ket mode or in the usual ket-bra mode)
+			#
+			if use_ket:
+				out_m_list = insideout_ket(T, in_m_list)
+			else:
+				out_m_list = insideout(T, in_m_list)
+			
+			#
+			# Normalize the messages and update the main list of messages
+			#
+			for l in range(legs_no):
+				e = edges_list[i][l]
+				(i1,j1) = vertices[e]
+				j = (i1 if i1 !=i else j1)
+				
+				err += norm(m_list[i][j] - out_m_list[l]/norm(out_m_list[l]))**2
+				
+				message = (1-damping)*out_m_list[l] + damping*m_list[i][j]
+				
+				message = message/norm(message)
+				
+				m_list[i][j] = message
+		
+		# 
+		# The error is the average L_2 distance divided by the total number
+		# of coordinates if we stack all messages as one huge vector
+		#
+		
+		err = sqrt(err)/n
+		
+		if log:
+			print("qbp iter {}: err = {}".format(iter_no,err))
+	
+
+	return m_list, err, iter_no
+	
+	
+	
